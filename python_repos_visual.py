10ece0979b7d7402153694bf4a12f87ed9d99579
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-12 00:50:00 -0600

    Add clickable links to graph
    - The names of each repo in the x-axis now are linked to the repo's url,
    so when we click on one of these names we are redirected to the correct
    project.

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -15,9 +15,14 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Process repository information.
 repo_dicts = response_dict["items"]
-repo_names, stars, hover_texts = [], [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict["name"])
+    # Turn repo names into active links.
+    repo_name = repo_dict["name"]
+    repo_url = repo_dict["html_url"]
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
     stars.append(repo_dict["stargazers_count"])
 
     # Build hover texts.
@@ -30,7 +35,7 @@ for repo_dict in repo_dicts:
 title = "Most-Starred Python Projects on GitHub"
 labels = {"x": "Repository", "y": "Stars"}
 fig = px.bar(
-    x=repo_names,
+    x=repo_links,
     y=stars,
     title=title,
     labels=labels,
