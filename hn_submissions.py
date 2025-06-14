be9234879e7349a3df0c8a0bbe7e109c410fd78f
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-13 23:58:57 -0600

    Update script to show a graph of the submissions
    - The script now shows a graph of the most popular submissions in Hacker
    Rank, based on the total number of comments. The title of each bar is a
    link to the original discussion of the submission.

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
 
+import plotly.express as px
 import requests
 
 
@@ -41,11 +42,38 @@ def main() -> None:
         reverse=True,
     )
 
-    # Print the title, the link, and the number of comments for each submission.
+    # Create the list for the axes of the graph.
+    submission_links, comments = [], []
     for submission_dict in submission_dicts:
-        print(f"\nTitle: {submission_dict['title']}")
-        print(f"Discussion link: {submission_dict['hn_link']}")
-        print(f"Comments: {submission_dict['comments']}")
+        # Only take into account submissions with more than 10 comments.
+        if (sub_comments := submission_dict["comments"]) > 10:
+            # Turn the submission title into an active link.
+            sub_title = submission_dict["title"]
+            sub_url = submission_dict["hn_link"]
+            sub_link = f"<a href='{sub_url}'>{sub_title}</a>"
+            submission_links.append(sub_link)
+
+            # Append the total number of comments.
+            comments.append(sub_comments)
+
+    # Make the visualization
+    title = "Most Active Discussions in Hacker Rank"
+    labels = {"x": "Submissions", "y": "Number of comments"}
+    fig = px.bar(
+        x=submission_links,
+        y=comments,
+        title=title,
+        labels=labels,
+    )
+
+    # Change the font settings.
+    fig.update_layout(
+        title_font_size=28,
+        xaxis_title_font_size=14,
+        yaxis_title_font_size=14,
+    )
+
+    fig.show()
 
 
 if __name__ == "__main__":
