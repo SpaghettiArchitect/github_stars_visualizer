0b3eb24baea382d625397dfecf2015de86d6cd16
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-29 01:40:05 -0600

    Add fixture annotations on each test

--- test_python_repos_visual.py
+++ test_python_repos_visual.py
@@ -9,19 +9,19 @@ def api_response():
     return get_github_data("python")
 
 
-def test_response_200(api_response):
+def test_response_200(api_response: tuple[int, dict]):
     """Check if the API call returns a status code of 200."""
     response_code, _ = api_response
     assert response_code == 200
 
 
-def test_complete_results(api_response):
+def test_complete_results(api_response: tuple[int, dict]):
     """Check if the API call returns complete results."""
     _, response_dict = api_response
     assert not response_dict["incomplete_results"]
 
 
-def test_more_than_10_repos_found(api_response):
+def test_more_than_10_repos_found(api_response: tuple[int, dict]):
     """Check that the API call has returned more than 10 items."""
     _, response_dict = api_response
     assert len(response_dict["items"]) > 10
