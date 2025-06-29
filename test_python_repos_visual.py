299919da212a7e75a9a29ba397192dc16a7f24ef
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-29 00:36:25 -0600

    Add test for the API call
    - The tests in test_python_repos_visual check the functionality of the
    get_github_data funtion on python_repos_visual, so we can be sure that
    the API query and data response is correct.

---
+++ test_python_repos_visual.py
@@ -0,0 +1,27 @@
+import pytest
+
+from python_repos_visual import get_github_data
+
+
+@pytest.fixture
+def api_response():
+    """The API response data that will be used in other tests."""
+    return get_github_data("python")
+
+
+def test_response_200(api_response):
+    """Check if the API call returns a status code of 200."""
+    response_code, _ = api_response
+    assert response_code == 200
+
+
+def test_complete_results(api_response):
+    """Check if the API call returns complete results."""
+    _, response_dict = api_response
+    assert not response_dict["incomplete_results"]
+
+
+def test_more_than_10_repos_found(api_response):
+    """Check that the API call has returned more than 10 items."""
+    _, response_dict = api_response
+    assert len(response_dict["items"]) > 10
